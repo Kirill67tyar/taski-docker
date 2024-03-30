9c4160044658db3e9e47653a09136742db7329ca
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2024-03-31 01:45:39 +0300

    asd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-
+# 
 POSTGRES_DB = os.getenv('POSTGRES_DB', 'django')
 POSTGRES_USER = os.getenv('POSTGRES_USER', 'django')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '')
